1e101ca6811097c0784b5d8367381f9288eb8957
Ammar Assem <ammar.assem@hotmail.com>
2024-04-16 22:57:20 +0300

    Add Custom

---
+++ resources/plugins-src/logger/client/client.js
@@ -0,0 +1,8 @@
+// See `client/client.js`
+
+import { registerBpmnJSPlugin } from 'camunda-modeler-plugin-helpers';
+
+import LoggingPlugin from './LoggingPlugin';
+
+// Register a plugin for bpmn-js
+registerBpmnJSPlugin(LoggingPlugin);
